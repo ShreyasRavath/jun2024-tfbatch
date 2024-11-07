2069b0b97c7068ca23f2ed583687713210ca8001
ShreyasRavath <34619797+ShreyasRavath@users.noreply.github.com>
2024-11-07 11:27:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIASOFCOLZ2ENZJZG5G"
-  secret_key = "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"
+  access_key = "AKIAV5Q3NR2DUJZRPENQ"
+  secret_key = "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"
 }
